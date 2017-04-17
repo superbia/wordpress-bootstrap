8f4a6f371d8e8e2eea10cb28a096eacb3b558f67
Dylan <dylan@superbia.com.au>
2017-04-17 21:00:30 +1000

    Move closing main tag above wp_footer().

--- public/app/themes/_s/footer.php
+++ public/app/themes/_s/footer.php
@@ -10,10 +10,10 @@
  */
 
 ?>
+	</main>
 	<?php if ( current_user_can( 'activate_plugins' ) ) : ?>
 		<div style="position:absolute;bottom:0;right:0;position:fixed;background-color:#000;color:#fff;font-family:Arial, sans-serif;font-size:9px;padding:5px;z-index:500;"><?php timer_stop(1); ?> seconds / <?php echo $wpdb->num_queries; ?> queries</div>
 	<?php endif; ?>
 	<?php wp_footer(); ?>
-	</main>
 </body>
 </html>
